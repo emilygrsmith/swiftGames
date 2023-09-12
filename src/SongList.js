eea0a83a1b84be3e9a9c17fb249df4ba2dc8a092
emilygrsmith <emilygrsmith11@gmail.com>
2023-09-11 23:41:18 -0400

    prettier

--- src/SongList.js
+++ src/SongList.js
@@ -8,6 +8,7 @@ function Home() {
   const [status,setStatus] = useState(false)
   const [gameEnded,setGameEnded] = useState(false);
   
+  
   const realSongs = ["tim mcgraw","picture to burn","teardrops on my guitar","a place in this world", "cold as you","the outside","tied together with a smile","stay beautiful","should've said no", "mary's song","our song","fearless","fifteen","love story","hey stephen","white horse","you belong with me","breathe","tell me why","you're not sorry","the way i loved you","forever and always","the best day","change","mine","sparks fly","back to december","speak now","dear john","mean","story of us","never grow up","enchanted","better than revenge","innocent","haunted","last kiss","long live","state of grace","red","treacherous","i knew you were trouble","all too well","22","i almost do","we are never ever getting back together","stay stay stay","the last time","holy ground","sad beautiful tragic","the lucky one","everything has changed","starlight","begin again","welcome to new york","blank space","style","out of the woods","all you had to do was stay","shake if off","i wish you would","bad blood","wildest dreams","how you get the girl","this love","i know places","clean",
 'ready for it','end game','i did something bad','dont blame me','delicate','look what you made me do','so it goes','gorgeous','getaway car','king of my heart','dancing with our hands tied','dress','this is why we cant have nice things','call it what you want','new years day','i forgot that you existed','cruel summer','lover','the man','the archer','i think he knows','miss americana and the heartbreak prince','paper rings','cornelia street','death by a thousand cuts','london boy','soon youll get better','false god','you need to calm down','afterglow','me','its nice to have a friend','daylight','the 1','cardigan','the last great american dynasty','exile','my tears ricochet','mirrorball','seven','august','this is me trying','illicit affairs','invisible string','mad woman','epiphany','betty','peace','hoax','willow','champagne problems','gold rush','tis the damn season','tolerate it','no body no crime','happiness','dorothea','coney island','ivy','cowboy like me','long story short','marjorie','closure','evermore','lavender haze','maroon','antihero','snow on the beach','youre on your own kid','midnight rain','question','vigilante shit','bejeweled','labyrinth','karma','sweet nothing','mastermind'];
 const [tableState, setTableState] = useState(Array(realSongs.length).fill(false));
@@ -81,6 +82,7 @@ const onClear = () => {
     <div>
         <div className = 'topScreen'>
       <h1>Can You Name All of Taylor Swift's Songs?</h1>
+      {!status ? <em><h3>Press Start to Begin</h3></em> : null}
       <div className = "submit">
       <form onSubmit={input}>
         <label>
@@ -93,9 +95,9 @@ const onClear = () => {
         <button className = "trySong" type="submit" >Enter</button>
       </form>
       </div> 
-      <button onClick={() => startGame()}>Start Game</button>
-      <button onClick={() => resetGame()}>Reset</button>
-      <button onClick={() => endGame()}>See Answers</button>
+      <button className = "startGame" onClick={() => startGame()}>Start Game</button>
+      <button className = "reset" onClick={() => resetGame()}>Reset</button>
+      <div><button className = "end" onClick={() => endGame()}>See Answers</button></div>
       <h1>Correct Count: {correctCount}/144</h1></div>
 <div className='one'>
   <div className="guessed-songs">
